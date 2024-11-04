8a8ede84b4fd481c264c2111bf51c4de07e37d14
nightFuryCodes <162005589+nightFuryCodes@users.noreply.github.com>
2024-11-03 23:47:21 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const cors = require("cors")
 
 const io = new Server(server, {
     cors:{
-        origin: 'http://localhost:5173', 
+        origin: 'https://react-and-websockets.vercel.app/', 
         methods: ['GET', 'POST'], 
         credentials: true 
     }
