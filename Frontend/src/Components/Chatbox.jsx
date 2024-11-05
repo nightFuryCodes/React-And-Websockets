e73eaccefa41a2d389cea29c9e9907e955f41e8a
nightFuryCodes <162005589+nightFuryCodes@users.noreply.github.com>
2024-11-05 13:28:42 -0500

    Update Chatbox.jsx

--- Frontend/src/Components/Chatbox.jsx
+++ Frontend/src/Components/Chatbox.jsx
@@ -15,7 +15,7 @@ const Chatbox = () => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [welcome, setWelcome] = useState([])
-  const user = "ashwin"
+  const user = sessionStorage.getItem("username")
   const navigate = useNavigate()
 
 
