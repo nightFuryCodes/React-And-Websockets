47e29baef926d088fabe3c88c6fce5a6130beffc
nightFuryCodes <162005589+nightFuryCodes@users.noreply.github.com>
2024-11-05 00:40:56 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -39,6 +39,11 @@ app.get("/login", (req, res)=>{
         msg: "loggedin"
     })
 })
+app.get("/", (req,res)=>{
+    res.json({
+        msg: "Let's Chat"
+        })
+})
 
 server.listen(3000, ()=>{
     console.log("server running at port 3000...")
