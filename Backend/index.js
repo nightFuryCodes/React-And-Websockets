4ab8123b2a4ccb0de889019c53e87573114d9efa
nightFuryCodes <162005589+nightFuryCodes@users.noreply.github.com>
2024-11-05 13:23:54 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,13 @@ const express = require("express")
 const http = require("http")
 const { Server } = require("socket.io")
 const app = express()
-const path = require("path")
 const { Socket } = require("dgram")
 const server = http.createServer(app)
 const cors = require("cors")
-const jwt = require("jsonwebtoken")
-const cookieParser = require("cookie-parser")
 
-const key = "ashwin"
 const io = new Server(server, {
     cors:{
-        origin: "http://localhost:5173", 
+        origin: "https://react-and-websockets-gamma.vercel.app", 
         methods: ['GET', 'POST'], 
         credentials: true 
     }
